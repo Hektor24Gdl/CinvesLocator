6a474b8780d8015561407c8a2b8ce9fef56fc830
marCel <m.celeste.ramtru@gmail.com>
2015-04-25 19:56:44 -0500

    Agregando avances de interfaz.  - Navegador  - Principal  - Login

---
+++ CinvesLocatorClient/app/src/main/java/mx/cinvestav/edu/cinveslocatorclient/menu1.java
@@ -0,0 +1,36 @@
+package mx.cinvestav.edu.cinveslocatorclient;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+/**
+ * Created by Celeste on 25/04/2015.
+ */
+public class menu1 extends Fragment {
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.menu1, container, false);
+        return rootview;
+    }
+}
---
+++ CinvesLocatorClient/app/src/main/java/mx/cinvestav/edu/cinveslocatorclient/menu5.java
@@ -0,0 +1,21 @@
+package mx.cinvestav.edu.cinveslocatorclient;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Celeste on 25/04/2015.
+ */
+public class menu5 extends Fragment {
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.menu5, container, false);
+        return rootview;
+    }
+}
