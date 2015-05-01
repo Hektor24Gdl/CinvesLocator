f8a04037788a0ad11c4f0d708f657cca157c814d
marCel <m.celeste.ramtru@gmail.com>
2015-05-01 02:55:35 -0500

    Se cambio codigo para que visualizacion general tuviera movimiento. RMIDummy ahora genera puntos aleatorios para que se distinga el movimiento

--- CinvesLocatorClient/app/src/main/java/mx/cinvestav/edu/cinveslocatorclient/menu5.java
+++ CinvesLocatorClient/app/src/main/java/mx/cinvestav/edu/cinveslocatorclient/menu5.java
@@ -17,6 +17,8 @@ import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import mx.cinvestav.edu.cinveslocatorclient.RPC.client.PointsProvider;
 import mx.cinvestav.edu.cinveslocatorclient.RPC.common.MapResource;
@@ -28,13 +30,88 @@ public class menu5 extends Fragment {
     View rootview;
     ImageView imageView;
     Bitmap mapa;
-
+    Timer timer;
+    TimerTask timerTask;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootview = inflater.inflate(R.layout.menu5, container, false);
         return rootview;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+          //onResume we start our timer so it can start when the app comes from the background
+        startTimer();
+    }
+    public void startTimer() {
+        //set a new Timer
+        timer = new Timer();
+        //initialize the TimerTask's job
+        initializeTimerTask();
+
+        //schedule the timer, after the first 5000ms the TimerTask will run every 10000ms
+        timer.schedule(timerTask, 1000, 1000); //
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stoptimertask(rootview);
+    }
+
+    public void stoptimertask(View v) {
+        //stop the timer, if it's not already null
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+    }
+    public void initializeTimerTask() {
+
+        timerTask = new TimerTask() {
+            public void run() {
+                getActivity().runOnUiThread(new Runnable() {
+                    public void run() {
+                        imageView = (ImageView) getView().findViewById(R.id.imageView6);
+                        //Create bitmap from mapa
+                        Bitmap img = BitmapFactory.decodeResource(getResources(), R.mipmap.mapa1);
+                        //Create a new bitmap where we will merge the image and the circle
+                        Bitmap bitmap = Bitmap.createBitmap(img.getWidth(), img.getHeight(), Bitmap.Config.RGB_565);
+                        //create canvas for bitmap
+                        Canvas c = new Canvas(bitmap);
+                        Paint paint = new Paint();
+                        paint.setColor(Color.BLUE);
+                        paint.setStrokeWidth(5.0f);
+                        // imageView.addt  ----------
+                        //-----------------------------------------------------------AQUI VOY
+                        c.drawBitmap(img, 0, 0, null);
+                        ArrayList<MapResource> puntos = PointsProvider.getInstance().getAll();
+                        for (MapResource mr : puntos) {
+                            if (mr.getType().equals("persona")) {
+                                paint.setColor(Color.BLUE);
+                            }
+                            if (mr.getType().equals("impresora")) {
+                                paint.setColor(Color.GREEN);
+                            }
+                            if (mr.getType().equals("laboratorio")) {
+                                paint.setColor(Color.MAGENTA);
+                            }
+                            c.drawCircle(mr.getPosition().getX(), mr.getPosition().getY(), 25.0F, paint);
+                        }
+                        //Now scale to imgView
+                        mapa = bitmap;
+                        //Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, imageView.getWidth(), imageView.getHeight(), false);
+                        imageView.setAdjustViewBounds(true);
+                        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                        imageView.setImageBitmap(mapa);
+                    }
+                });
+            }
+        };
+    }
+
     public void onActivityCreated(Bundle savedInstanceState){
         // setView(R.layout.activity_login);
         super.onActivityCreated(savedInstanceState);
--- RPCServer/src/rmi/RMIDummy.java
+++ RPCServer/src/rmi/RMIDummy.java
@@ -4,12 +4,16 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Random;
+
+import common.MapResource;
 
 public class RMIDummy {
 	public static void main(String args[]){
 		DBConnector rmiObject;
 		try {
 			rmiObject = (DBConnector)Naming.lookup("//127.0.0.1/DBConnectorRemoto");
+			rmiObject.setUbicacion("Celeste", "persona", 1, 50, 50);
 			rmiObject.setUbicacion("Hector", "persona", 1, 831, 932);
 			rmiObject.setUbicacion("Gibran", "persona", 1, 666, 666);
 			rmiObject.setUbicacion("Lea", "persona", 1, 555, 555);
@@ -19,6 +23,31 @@ public class RMIDummy {
 			rmiObject.setUbicacion("Lab2", "laboratorio", 1, 532, 231);
 			rmiObject.setUbicacion("impre-Lab3", "impresora", 1, 352, 832);
 			rmiObject.setUbicacion("impre-Lab4", "impresora", 1, 355, 233);
+			String[] names = {"Hector","Gibran","Lea","Victor","Felix","Lab1","Lab2","impre-Lab3","impre-Lab4"};
+			int[] direccion={1,1,1,1,1,1,1,1,1,};
+			Random generator = new Random();
+			while(true){
+				try {
+					MapResource act = null;;
+					for(int i=0;i<names.length;i++){
+						act = rmiObject.getUbicacion(names[i]);
+						int nx,ny;
+						int dirX = generator.nextInt() %3;
+						int dirY = generator.nextInt() %3;
+						if(dirX == 2 ) dirX = -1;
+						if(dirY == 2 ) dirY = -1;
+						do{
+							nx = act.getPosition().getX() + generator.nextInt(20) * dirX;
+							ny = act.getPosition().getX() + generator.nextInt(20) * dirY;
+						}while( nx > 1200 || ny > 1200 || nx <= 0 || ny <= 0);
+						rmiObject.setUbicacion(act.getName(), act.getType(), act.getPlanta(), nx,ny);
+					}
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		} catch (MalformedURLException | RemoteException | NotBoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
